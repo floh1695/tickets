0706e0e3faf8067b3196ab64f1c5c138f2c02a3b
Charles Bayley <cbayley95@gmail.com>
2019-02-07 22:27:36 -0500

    My attempts at working with prompt * I think I need to find a way to convert a UserData response into a proper functor

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
 'use strict';
 
-const welcomeMessage = 'Welcome to Tickets!';
-console.log();
+const prompt = require('prompt');
+
+//* ()
+const welcome = () => {
+  const welcomeMessage = 'Welcome to Tickets!';
+  console.log(welcomeMessage);
+}
+
+const askForUserData = () => {
+  const schema = {
+    properties: {
+      username: {
+        pattern: /^[a-zA-Z]+$/,
+        message: 'Userame may only be letters',
+        require: true,
+      },
+      password: {
+        pattern: /^[a-zA-Z]+$/,
+        message: 'Password may only be letters',
+        hidden: true,
+        required: true,
+      },
+    }
+  };
+
+  const map = f => {
+    prompt.start();
+
+    const lock = {};
+    let result = lock;
+    prompt.get(schema, (_, r) => result = f(r));
+    while (result === lock) {
+      /* Wait until we recieve a result */
+    }
+    return {
+      map: f => f();
+    }
+  };
+
+  const userData = {
+    map,
+  };
+
+  return userData;
+};
+
+const main = () => {
+  welcome();
+  askForUserData()
+    .map(userData => {
+      console.log(userData);
+    });
+};
+
+main();
 
